f52d1cac3f07cfb4af62f05f8796552cc877f300
mohan-sys <mohanraj123.mrl@gmail.com>
2022-03-11 09:15:56 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
      <h1>hello</h1>
-     <p>this is a sample, testing js  </p>
+     <p>this is a sample, testing js</p>
     
     </div>
   );
