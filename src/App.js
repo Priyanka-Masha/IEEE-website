b4e19c62a448b1ae2c4ad325cc4bab0b72fd8bd1
Prith Kami <prithvi40201@gmail.com>
2022-03-14 17:12:23 +0530

    routing pages plus header

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
-import Header from './components/Header'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Body from './components/Body';
-import About from './components/About';
-import Excom19 from './components/pages/Excom/Excom19';
-import Excom21 from './components/pages/Excom/Excom21';
-import Excom20 from './components/pages/Excom/Excom20';
-import antenna from './components/pages/Society/antenna';
-import Society from './components/pages/Society/Society';
-import AboutUs from './components/pages/AboutUs';
-import Events20 from './components/pages/Events/Events20';
-
-import Events21 from './components/pages/Events/Events21';
-import Events22 from './components/pages/Events/Events22';
-import Membership from './components/pages/Membership';
-import Contact from './components/pages/Contact';
-
-import SignIn from './components/pages/SignIn';
-
+import Routerss from "./Routerss";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<><Header/><Body/><About/></>}/>
-        <Route exact path="/AboutUs" element={<><Header/><AboutUs/></>}/>
+    <div>
+      <Routerss />
+    </div>
+  );
+};
 
-        <Route exact path="/Excom" element={<><Header/><Excom19/><Excom20/><Excom21/></>}/>
-        <Route exact path="/Societies" element={<><Header/><Society/></>}/>
-
-        <Route exact path="/Events" element={<><Header/><Events20/><Events21/><Events22/></>}/>
-        <Route exact path="/Membership" element={<><Header/><Membership/></>}/>
-
-        <Route exact path="/Gallery" element={<><Header/></>}/>
-        <Route exact path="/ContactUs" element={<><Header/><Contact/></>}/>
-        <Route exact path="/SignIn" element={<><Header/><SignIn/></>}/>
-     
-
-   
-
-
-       
-
-      </Routes>
-  </Router>
-
-  )
-}
-
-export default App
+export default App;
--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const AboutUs = () => {
   return (
     <div>
-       <h1>
-      ---------------------------------------------🥷🥷🥷🥷🥷🥷🥷--------------------------------------
-
-      </h1>
-      <h1>About us page from home page.........with some extra stuffffffffffffffffffffffff</h1>
       <h1>
-      ---------------------------------------------🥷🥷🥷🥷🥷🥷🥷--------------------------------------
-
+        ---------------------------------------------🥷🥷🥷🥷🥷🥷🥷--------------------------------------
       </h1>
-      </div>
-  )
-}
+      <h1>
+        About us page from home page.........with some extra
+        stuffffffffffffffffffffffff
+      </h1>
+      <h1>
+        ---------------------------------------------🥷🥷🥷🥷🥷🥷🥷--------------------------------------
+      </h1>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
